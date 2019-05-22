d878f4b31596b48534be05968f02c4154f66c7d0
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-22 11:37:10 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,11 +18,9 @@ end
 def get_user_input
   input = gets.chomp
   return input
-  # code #get_user_input here
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
