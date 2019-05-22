71d549392ed44df9163c4342deb7a7bd724670b6
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-22 11:33:13 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,8 +7,8 @@ def deal_card
   return rand
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
